99f2307223dbd287c5a9db65c3ff6a03e859fecf
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-10-21 23:34:31 +0530

    session middleware done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,26 @@
+"use client";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex gap-4">
-      <Input/>
-      <Button >
-         Primary
-      </Button>
-      <Button variant={"secondary"}>
-        Secondary
-      </Button>
-      <Button variant={"destructive"}>
-         Destructive 
-      </Button>
-      <Button variant={"ghost"}>
-         Ghost 
-      </Button>
-      <Button variant={"muted"}>
-         Muted 
-      </Button>
-      <Button variant={"outline"}>
-         Outline 
-      </Button>
-      <Button variant={"teritary"}>
-         Teritary
-      </Button>
+  const router = useRouter();
 
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+  return (
+    <div>
+      only visible to authorized users.{" "}
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -6,7 +6,12 @@ import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
 import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
+import { sessionMiddleware } from "@/lib/session-middleware";
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
     const { account } = await createAdminClient();
@@ -39,9 +44,12 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post("/logout",(c)=>{
-    deleteCookie(c,AUTH_COOKIE)
-    return c.json({success:true})
-  })
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
+    deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
+    return c.json({ success: true });
+  });
 
 export default app;
